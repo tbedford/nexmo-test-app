fe639f4ca492165be84014fc7b9d176e2e125b75
Tony Bedford <tony.bedford@vonage.com>
2019-05-15 09:46:30 +0100

    Initial version

---
+++ server.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from pprint import pprint
+
+app = Flask(__name__)
+
+audio_url = "https://raw.githubusercontent.com/tbedford/git-testing-repo/master/tunes/Komiku_Sunset_on_the_beach.mp3"
+
+ncco = [
+    {
+        "action": "stream",
+        "loop": 0,
+        "streamUrl": [audio_url]
+    }
+]
+
+app = Flask(__name__)
+
+@app.route("/webhooks/answer")
+def answer_call():
+    params = request.args
+    pprint(params)
+    return jsonify(ncco)
+
+@app.route("/webhooks/event", methods=['POST'])
+def events():
+    data = request.get_json()
+    pprint(data)
+    return ("200")
+
+@app.route('/webhooks/inbound', methods=['POST'])
+def inbound_message():
+    print ("** inbound_message **")
+    data = request.get_json()
+    pprint(data)
+    return ("inbound_message", 200)
+
+@app.route('/webhooks/status', methods=['POST'])
+def message_status():
+    print ("** message_status **")
+    data = request.get_json()
+    pprint(data)
+    return ("message_status", 200)
+
+@app.route('/webhooks/inbound-sms', methods=['POST'])
+def inbound_sms():
+    print ("** inbound_sms **")
+    values = request.values
+    pprint(values)
+    return ("inbound_sms", 200)
+
+@app.route('/webhooks/delivery-receipt', methods=['POST'])
+def delivery_receipt():
+    print ("** delivery_receipt **")
+    data = request.get_json()
+    pprint(data)
+    return ("delivery_receipt", 200)
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=9000)
