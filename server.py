39d08464209aa29bf749e5020e9fd8095a7d9e7e
Tony Bedford <tony.bedford@vonage.com>
2019-10-17 10:58:47 +0100

    Update server.py

--- server.py
+++ server.py
@@ -39,34 +39,6 @@ def events():
     pprint(data)
     return ("200")
 
-@app.route('/webhooks/inbound', methods=['POST'])
-def inbound_message():
-    print ("** inbound_message **")
-    data = request.get_json()
-    pprint(data)
-    return ("inbound_message", 200)
-
-@app.route('/webhooks/status', methods=['POST'])
-def message_status():
-    print ("** message_status **")
-    data = request.get_json()
-    pprint(data)
-    return ("message_status", 200)
-
-@app.route('/webhooks/inbound-sms', methods=['POST'])
-def inbound_sms():
-    print ("** inbound_sms **")
-    values = request.values
-    pprint(values)
-    return ("inbound_sms", 200)
-
-@app.route('/webhooks/delivery-receipt', methods=['POST'])
-def delivery_receipt():
-    print ("** delivery_receipt **")
-    data = request.get_json()
-    pprint(data)
-    return ("delivery_receipt", 200)
-
 if __name__ == '__main__':
     load_dotenv()
     app.run(host="localhost", port=9000)
